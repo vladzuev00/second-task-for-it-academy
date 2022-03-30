7a4b934d5290ab0c4879881b6486986ae5f15eb9
vladzuev00 <80713484+vladzuev00@users.noreply.github.com>
2022-03-30 21:43:47 +0300

    Delete dao directory

--- dao/cryptographer/Cryptographer.java
+++
@@ -1,7 +0,0 @@
-package by.itacademy.zuevvlad.cardpaymentproject.dao.cryptographer;
-
-public interface Cryptographer<TypeOfNeededEncryptingData, TypeOfNeedDecryptingData>
-{
-    public abstract TypeOfNeedDecryptingData encrypt(final TypeOfNeededEncryptingData encryptedData);
-    public abstract TypeOfNeededEncryptingData decrypt(final TypeOfNeedDecryptingData decryptedData);
-}
